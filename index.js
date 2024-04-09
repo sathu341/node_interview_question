e7d7c094818f7d1927f35b253c40b066274b5ad0
sathu341 <77712619+sathu341@users.noreply.github.com>
2024-04-09 11:05:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express=require('express')
+const app=express()
+
+//routes
+app.get("/",(req,res)=>{
+    res.send("Hello Expressjs")
+})
+app.get("/about",(req,res)=>{
+    res.send("About page is running")
+})
+app.get("/product/:id",(req,res)=>{
+    const prdid=req.params.id
+    res.send(prdid)
+})
+app.get("/user",(req,res)=>{
+    const user=req.query.search
+    res.send(user)
+})
+app.listen(8081,function(){
+    console.log("server running port:8081")
+})
